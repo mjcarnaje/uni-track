3de600de5a9d53846d6bd172debeba145594ac34
mjcarnaje <michaeljamescarnaje1@gmail.com>
2023-09-24 15:18:08 +0800

    chore: add tailwindcss

--- ssis/views.py
+++ ssis/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
   return render_template("home.html")
+
+@views.route('/dashboard')
+def dashboard():
+  return render_template("dashboard.html")
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./ssis/templates/**/*.{html,htm}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
