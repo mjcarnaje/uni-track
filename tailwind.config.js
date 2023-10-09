5288e05169630356d60a3e986241496425250488
mjcarnaje <michaeljamescarnaje1@gmail.com>
2023-10-10 07:06:56 +0800

    chore: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   mode: "jit",
   content: ["./ssis/templates/**/*.{html,htm}"],
   theme: {
+    fontFamily: {
+      display: ['"Mochiy Pop P One"'],
+      body: ["Inter"],
+    },
     extend: {},
   },
   plugins: [],
